141ea3048ff7c7b7507f055f5466b525c75985e1
Souren Papazian <souren@papazian.info>
2020-04-23 16:54:32 -0400

    Base for bitwise and of numbers range

---
+++ 30_day_challenge/solutions_cpp/src/bitwise_and_of_numbers_range.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+namespace BitwiseAndOfNumbersRange {
+    class Solution {
+    public:
+        int rangeBitwiseAnd(int m, int n) {
+            return 5 & 3;
+        }
+    };
+
+    void printOutput(int output) {
+        std::cout << output << std::endl;
+    }
+}
